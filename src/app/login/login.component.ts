770a8d8f90f78807fece74b72feb918da5c8720e
Gilson Bezerra <gilson.bezerra@live.com>
2024-10-29 10:33:10 -0300

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+
+  async onSubmit() {
+    try {
+      await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
+      // Redirecionar após login
+      this.router.navigate(['/home']); // Altere para a rota correta
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
+  }
+
+  goToSignup() {
+    this.router.navigate(['/signup']); // Altere para a rota de cadastro
+  }
 }
+
